aabef641ce18b2df406ad326666f7359823fe185
Edwin Parra <Edprr4@gmail.com>
2017-01-17 09:23:46 -0500

    Update case_solution.rb

--- case_solution.rb
+++ case_solution.rb
@@ -20,11 +20,11 @@ puts "So what would you like to buy?"
 choice = gets.chomp.upcase
 case 
 when (choice == "SHIRT"   &&(money - shirt >= 0) )
-  puts "ok, here's your shirt for #{shirt}. Your change is $#{money - shirt}"
+  puts "ok, here's your shirt for $#{shirt}. Your change is $#{money - shirt}"
 when (choice == "PANTS"   &&(money - pants >= 0) )
-  puts "ok, here's your pants for #{pants}. Your change is $#{money - pants}"
+  puts "ok, here's your pants for $#{pants}. Your change is $#{money - pants}"
 when (choice == "SOCKS" &&(money - socks >= 0) )
-  puts "ok, here are your socks for #{socks}. Your change is $#{money - socks}"
+  puts "ok, here are your socks for $#{socks}. Your change is $#{money - socks}"
   #needs a fix for pants and socks
 when ( choice !=  ("SHIRT" || "PANTS" || "SOCKS"))
 puts "wrong choice! We don't have that!!!"
